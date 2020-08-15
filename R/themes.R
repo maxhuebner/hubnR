fc37cc2b76a5b202e9761012a567b18caacd492e
Max Huebner <max.huebner36@gmail.com>
2020-08-15 18:49:18 +0200

    Added new Themes

--- R/themes.R
+++ R/themes.R
@@ -3,7 +3,7 @@
 #' @param base_size Base Size For Plot Text
 #' @param font Font to Use
 #' @export
-theme_hubnr <- function(base_size = 12, font = "Roboto") {
+theme_hubnr <- function(base_size = 11, font = "Roboto") {
 
   # Text setting
   txt <- ggplot2::element_text(size = base_size + 2, colour = "black", face = "plain")
@@ -58,5 +58,94 @@ theme_hubnr <- function(base_size = 12, font = "Roboto") {
 #' @export
 theme_hubnr_thin <- function(base_size = 11, font = "Roboto Thin") {
   theme_hubnr(base_size = base_size, font = font)
+}
+
+
+#' Minimal ggplot2 theme using the Roboto Condensed and Roboto Bold fonts
+#'
+#' See: \url{https://github.com/juliasilge/silgelib}
+#'
+#' @param base_size base font size
+#' @param strip_text_size,strip_text_margin plot strip text size and margin
+#' @param subtitle_size,subtitle_margin plot subtitle size and margin
+#' @param plot_title_size,plot_title_margin plot title size and margin
+#' @param ... Other arguments passed to \code{theme_minimal}
+#'
+#' @details The Roboto Condensed and Roboto Bold fonts are both Google fonts;
+#' they can be found at \url{https://fonts.google.com/specimen/Roboto+Condensed}
+#' and \url{https://fonts.google.com/specimen/Roboto}. These fonts must be
+#' installed locally on your computer for this theme to work.
+#'
+#' @export
+theme_roboto <- function(base_size = 11,
+                         strip_text_size = 12,
+                         strip_text_margin = 5,
+                         subtitle_size = 13,
+                         subtitle_margin = 10,
+                         plot_title_size = 16,
+                         plot_title_margin = 10,
+                         ...) {
+  ret <- ggplot2::theme_minimal(base_family = "RobotoCondensed-Regular",
+                                base_size = base_size, ...)
+  ret$strip.text <- ggplot2::element_text(
+    hjust = 0, size = strip_text_size,
+    margin = ggplot2::margin(b = strip_text_margin),
+    family = "Roboto-Bold"
+  )
+  ret$plot.subtitle <- ggplot2::element_text(
+    hjust = 0, size = subtitle_size,
+    margin = ggplot2::margin(b = subtitle_margin),
+    family = "RobotoCondensed-Regular"
+  )
+  ret$plot.title <- ggplot2::element_text(
+    hjust = 0, size = plot_title_size,
+    margin = ggplot2::margin(b = plot_title_margin),
+    family = "Roboto-Bold"
+  )
+  ret
+}
+
+
+#' Minimal ggplot2 theme using the IBM Plex Sans fonts
+#'
+#' See: \url{https://github.com/juliasilge/silgelib}
+#'
+#' @param base_size base font size
+#' @param strip_text_size,strip_text_margin plot strip text size and margin
+#' @param subtitle_size,subtitle_margin plot subtitle size and margin
+#' @param plot_title_size,plot_title_margin plot title size and margin
+#' @param ... Other arguments passed to \code{theme_minimal}
+#'
+#' @details The IBM Plex fonts are open source and can be found at
+#' \url{https://ibm.github.io/type/}. These fonts must be installed locally on
+#' your computer for this theme to work.
+#'
+#' @export
+theme_plex <- function(base_size = 11,
+                       strip_text_size = 12,
+                       strip_text_margin = 5,
+                       subtitle_size = 13,
+                       subtitle_margin = 10,
+                       plot_title_size = 16,
+                       plot_title_margin = 10,
+                       ...) {
+  ret <- ggplot2::theme_minimal(base_family = "IBMPlexSans",
+                                base_size = base_size, ...)
+  ret$strip.text <- ggplot2::element_text(
+    hjust = 0, size = strip_text_size,
+    margin = ggplot2::margin(b = strip_text_margin),
+    family = "IBMPlexSans-Medium"
+  )
+  ret$plot.subtitle <- ggplot2::element_text(
+    hjust = 0, size = subtitle_size,
+    margin = ggplot2::margin(b = subtitle_margin),
+    family = "IBMPlexSans"
+  )
+  ret$plot.title <- ggplot2::element_text(
+    hjust = 0, size = plot_title_size,
+    margin = ggplot2::margin(b = plot_title_margin),
+    family = "IBMPlexSans-Bold"
+  )
+  ret
 }
 

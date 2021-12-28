bb53b51591770a0864cc92e98dc004297506eadf
Max Hübner <max.huebner36@gmail.com>
2021-12-28 19:56:45 +0100

    Minor cleanup and passthrough args for ggsave

--- R/save_plots.R
+++ R/save_plots.R
@@ -2,22 +2,29 @@
 
 #' Standard Export Format PDF
 #'
+#' @seealso ggplot2::ggsave
+#'
 #' Saves PDF with Fonts using Cairo_PDF in a Standard Format
 #' @param plot Plot Object
 #' @param name File Name - USE PDF FILE ENDING
 #' @export
-save_std_pdf <- function(plot, name, width = 8, height = 6, units = "in") {
-    ggplot2::ggsave(plot, filename = name, device = cairo_pdf, width = width, height = height, 
-        units = units)
+save_std_pdf <- function(plot, name, width = 8, height = 6, units = "in", dpi = 300, ...) {
+    ggplot2::ggsave(plot, filename = name,
+                    device = cairo_pdf,width = width,
+                    height = height, units = units, dpi = dpi, ...)
 }
 
 
 #' Standard Export Format PNG - Bitmap
 #'
+#' @seealso ggplot2::ggsave
+#'
 #' Saves PNG with Fonts in a Standard Format
 #' @param plot Plot Object
 #' @param name File Name - USE PNG FILE ENDING
 #' @export
-save_std_png <- function(plot, name, width = 8, height = 6, units = "in") {
-    ggplot2::ggsave(plot, filename = name, width = width, height = height, units = units)
+save_std_png <- function(plot, name, width = 8, height = 6, units = "in", dpi = 300, ...) {
+    ggplot2::ggsave(plot, filename = name,
+                    width = width, height = height,
+                    units = units, dpi = dpi, ...)
 }
--- R/startup.R
+++ R/startup.R
@@ -1,11 +1,6 @@
-# Hello, world!  This is an example function named 'hello' which prints 'Hello, world!'.
-# You can learn more about package authoring with RStudio at: http://r-pkgs.had.co.nz/
-# Some useful keyboard shortcuts for package authoring: Install Package: 'Ctrl + Shift +
-# B' Check Package: 'Ctrl + Shift + E' Test Package: 'Ctrl + Shift + T'
 
 .onLoad <- function(libname, pkgname) {
-    packageStartupMessage("hubnR utility and themes loaded.\n
-                          Use formatR::tidy_dir(\"R\") often!")
+    packageStartupMessage("hubnR personal package loaded.")
 }
 
 setup_font_use <- function(dev = "win", redo = F) {
